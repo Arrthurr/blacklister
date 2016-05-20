94d01895555153c599d9bd9f4f418e0ade044b5e
Arthur Turnbull <arthur.turnbull@gmail.com>
2016-05-20 17:28:58 -0500

    Update conn3Sixteen.php
    removed database admin user credentials

--- conn3Sixteen.php
+++ conn3Sixteen.php
@@ -4,8 +4,8 @@
 # HTTP="true"
 $hostname_conn3Sixteen = "localhost";
 $database_conn3Sixteen = "contacts";
-$username_conn3Sixteen = "arturo019";
-$password_conn3Sixteen = "osman019";
+$username_conn3Sixteen = "";
+$password_conn3Sixteen = "";
 $conn3Sixteen = mysql_pconnect($hostname_conn3Sixteen, $username_conn3Sixteen, $password_conn3Sixteen) or die(mysql_error());
 $db = mysql_select_db($database_conn3Sixteen, $conn3Sixteen);
 ?>
